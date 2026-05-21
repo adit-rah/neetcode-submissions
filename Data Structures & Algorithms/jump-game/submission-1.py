f09ee7554d8fd201e2366c9eb60cb17325b59d80
Adit <157396440+adit-rah@users.noreply.github.com>
2026-05-21 14:59:04 -0400

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if not nums:
+            return True
+
+        start = len(nums) - 1
+
+        dist = 1
+        for i in range(start-1, -1, -1):
+            if nums[i] >= dist:
+                dist = 0
+            dist += 1
+
+        return dist == 1 
