7c9e57665813cce132f779f8345fbcca86a73bb5
Adit <157396440+adit-rah@users.noreply.github.com>
2026-09-24 15:57:32 -0400

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        adj = {}
+
+        def find(x):
+            if x not in adj:
+                adj[x] = x
+            
+            if x != adj[x]:
+                adj[x] = find(adj[x])
+            
+            return adj[x]
+        
+        res = [-1,-1]
+        for a,b in edges:
+            pa,pb = find(a), find(b)
+
+            if pa == pb:
+                res = [a,b]
+            adj[pb] = pa
+    
+        
+        return res
